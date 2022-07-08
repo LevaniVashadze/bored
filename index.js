c719b00f79d79ab252384260e51b7f3ee56d2f26
LevaniVashadze <100613979+LevaniVashadze@users.noreply.github.com>
2022-07-08 12:44:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://www.boredapi.com/api/activity')
+    fetch('http://www.boredapi.com/api/activity')
         .then(response => response.json())
         .then(data => {
             document.querySelector('h1').innerHTML = data.activity
